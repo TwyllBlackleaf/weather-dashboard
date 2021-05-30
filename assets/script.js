591ca3d521313c98f51b82d5cea3cd7fa702b76a
Twyll <twyllblackleaf@gmail.com>
2021-05-30 07:18:20 -0400

    fetch geocoding and weather data

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,7 @@ var searchHistory = [];
 // Document Elements
 var searchFormEl = document.querySelector("#search-form");
 var searchInputEl = document.querySelector("#search-input");
+var cityNameEl = document.querySelector("#city-name");
 
 // Functions
 var searchWeather = function(event) {
@@ -19,24 +20,50 @@ var searchWeather = function(event) {
     console.log("search");
 
     // Get the input from #search-input
+    cityObj.input = searchInputEl.value;
+    console.log(cityObj.input);
 
     // Geocode it to get latitude and longitude
+    fetch(`https://api.geocod.io/v1.6/geocode?q=${cityObj.input}&api_key=21d7d19560980625866b0b6b82569b6b66b15d6&limit=1`)
+        .then(function(response) {
+            response.json()
+                .then(function(data) {
+                    //to do: make sure something actually got returned
 
-    // append the search term, city name, lat, and long to the array of search history buttons
+                    cityObj.cityName = data.results[0].address_components.city; // to do: add error checking to make sure there IS a city
+                    cityObj.lat = data.results[0].location.lat;
+                    cityObj.long = data.results[0].location.lng;
 
-    // store the array in localStorage
+                     // append the search term, city name, lat, and long to the array of search history buttons
 
-    // call getWeather with said city, latitude, and longitude
+                    // store the array in localStorage
+
+                    // call getWeather with said city, latitude, and longitude
+                    getWeather(cityObj.cityName, cityObj.lat, cityObj.long);
+                }) 
+        })
+
+   
 }
 
 var getWeather = function(city, lat, long) {
     // set the city name to display in relevant places
+    cityNameEl.textContent = city;
 
     // Use lat and long to find weather for a location
+    fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,alerts&appid=b24e2873602c0d7d47d5fbf2c9c9524b`)
+        .then(function(response) {
+            response.json()
+                .then(function(data) {
+                    console.log(data);
 
-    // Display current weather in current-weather div
+                    // Display current weather in current-weather div
 
-    // Display forecast in forecast cards
+                    // Display forecast in forecast cards
+                })
+        })
+
+    
 }
 
 var savedWeather = function() {
